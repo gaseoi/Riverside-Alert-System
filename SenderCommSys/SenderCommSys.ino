b01ef5b0b38a7e97b54e6e81a1190a905aeb5fa1
Jiarui Miao <158626875+gaseoi@users.noreply.github.com>
2025-09-25 13:49:31 +0100

    Update SenderCommSys.ino

--- SenderCommSys/SenderCommSys.ino
+++ SenderCommSys/SenderCommSys.ino
@@ -1,73 +1,128 @@
-#include <esp_now.h>
+#define dropButton 33
+#define drinkButton 26
+#define flatButton 23
+#define careButton 17
+#define reminderButton 18
+#define cancelButton 14
+#define buzzerPin 13
+
+volatile unsigned long lastInterruptTime = 0;
+const int cooldown = 1000; //Set maximum cooldown between transmissions
+const int debouncetime = 20;
+
 #include <WiFi.h>
+#include <ESPmDNS.h>
 
-#define CHANNEL 1
+//Change depending on wifi network name and password
+const char* ssid = "Name";
+const char* password =  "Password";
+  
+#include <WiFiUdp.h>
+WiFiUDP udp;
+char packetBuffer[255];
+unsigned int localPort = 9999;
+unsigned int serverport = 8888;
+IPAddress serverIp;
 
-/** This is all the data about the peer **/
-esp_now_peer_info_t slave;
-
-/** The all important data! **/
-uint8_t data = 0;
-
-int buttonPin = 12;
-int buzzerPin = 13;
+bool expectReply = false;
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  esp_now_init();
-  esp_now_register_send_cb(OnDataSent);
-  ScanForSlave(); // WiFi.macAddress()
-  esp_now_add_peer(&slave);
-
-  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(dropButton, INPUT_PULLUP);
+  pinMode(drinkButton, INPUT_PULLUP);
+  pinMode(flatButton, INPUT_PULLUP);
+  pinMode(careButton, INPUT_PULLUP);
+  pinMode(reminderButton, INPUT_PULLUP);
+  pinMode(cancelButton, INPUT_PULLUP);
 
+  Serial.begin(115200);
+   
+  WiFi.begin(ssid, password);
+     
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+     
+  while(!MDNS.begin("sender1")) {
+    Serial.println("Starting mDNS...");
+    delay(1000);
+  }
+ 
+  Serial.println("MDNS started");
+ //following is necessary to extract the other esp's ip address
+ 
+  while (serverIp.toString() == "0.0.0.0") {
+    Serial.println("Resolving host...");
+    delay(250);
+    serverIp = MDNS.queryHost("commsys");
+  }
+ 
+  Serial.println("Host address resolved:");
+  Serial.println(serverIp.toString());   
+ //initiate udp
+  udp.begin(localPort);
+  Serial.printf("UDP Client : %s:%i \n", WiFi.localIP().toString().c_str(), localPort);
+}
+   
+void transmit(char sendmessage[]){
+  //for sending udp data
+  udp.beginPacket(serverIp, serverport);
+  //Serial.println("transmitted message");
+  udp.printf(sendmessage);
+  udp.endPacket();
+}
+
+void msgSend() {
+  expectReply = true;
+  lastInterruptTime = millis();
+  tone(buzzerPin, 1000, 1000); 
 }
 
 void loop() {
-  int buttonValue = digitalRead(buttonPin);
-  if (buttonValue == LOW){
-    esp_now_send(slave.peer_addr, &data, sizeof(data));
-    data = 1;
-    Serial.print("BUZZ ");
-    digitalWrite(buzzerPin, HIGH);
-    delay(200);
+  //for recieving udp data
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    Serial.print(" Received packet from : "); 
+    Serial.println(udp.remoteIP());
+    int len = udp.read(packetBuffer, 255);
+    Serial.printf("Data : %s\n", packetBuffer);
+    Serial.println();
   }
-  else{
-    digitalWrite(buzzerPin, LOW);
-    esp_now_send(slave.peer_addr, &data, sizeof(data));
-    data = 0;
-    delay(200);
+
+  unsigned long currentTime = millis();
+  if ((currentTime - lastInterruptTime >= cooldown) && digitalRead(dropButton) == LOW){
+    transmit("Dropped object");
+    msgSend();
   }
-}
-
-/** Scan for slaves in AP mode and ad as peer if found **/
-void ScanForSlave() {
-  int8_t scanResults = WiFi.scanNetworks();
-
-  for (int i = 0; i < scanResults; ++i) {
-    String SSID = WiFi.SSID(i);
-    String BSSIDstr = WiFi.BSSIDstr(i);
-
-    if (SSID.indexOf("RX") == 0) {
-
-      int mac[6];
-      if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
-        for (int ii = 0; ii < 6; ++ii ) {
-          slave.peer_addr[ii] = (uint8_t) mac[ii];
-        }
-      }
-
-      slave.channel = CHANNEL; // pick a channel
-      slave.encrypt = 0; // no encryption
-      break;
-    }
+  if ((currentTime - lastInterruptTime >= cooldown) && digitalRead(drinkButton) == LOW){
+    transmit("Drink needed");
+    msgSend();
   }
-}
-
-/** callback when data is sent from Master to Slave **/
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("I sent my data -> ");
-  Serial.println(data);
+  if ((currentTime - lastInterruptTime >= cooldown) && digitalRead(flatButton) == LOW){
+    transmit("Help returning to flat");
+    msgSend();
+  }
+  if ((currentTime - lastInterruptTime >= cooldown) && digitalRead(careButton) == LOW){
+    transmit("General care needed");
+    msgSend();
+  }
+  if ((currentTime - lastInterruptTime >= cooldown) && digitalRead(reminderButton) == LOW){
+    transmit("Event reminder");
+    msgSend();
+  }
+  if ((currentTime - lastInterruptTime >= debouncetime) && digitalRead(cancelButton) == LOW){
+    transmit("Sender cancelled");
+    expectReply = false;
+    lastInterruptTime = millis();
+  }
+  
+  if (strcmp(packetBuffer, "All good") == 0 && expectReply == true) {
+    expectReply = false;
+    tone(buzzerPin, 2000);
+    delay(1000);
+    noTone(buzzerPin);
+    Serial.println("All good (formally acknow) recieved");
+    packetBuffer[0] = '\0';
+  }
 }
